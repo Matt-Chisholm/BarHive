33667af2150f0377de3880c73566aec844642e40
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-03-17 09:26:18 -0700

    search working

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,23 +8,23 @@ import Feather from "react-native-vector-icons/Feather";
 export default function SearchScreen() {
   const [results, setResults] = React.useState([]);
   const [searchTerm, setSearchTerm] = React.useState("");
-
-  const options = {
-    method: "GET",
-    url: "https://the-cocktail-db.p.rapidapi.com/filter.php",
-    params: { i: searchTerm },
-    headers: {
-      "x-rapidapi-key": API_KEY,
-      "x-rapidapi-host": API_HOST,
-    },
-  };
+  const [inputValue, setInputValue] = React.useState("");
 
   const search = () => {
+    const formattedSearchTerm = inputValue.trim().toLowerCase();
+    setSearchTerm(formattedSearchTerm);
     axios
-      .request(options)
+      .get(
+        `https://the-cocktail-db.p.rapidapi.com/search.php?i=${formattedSearchTerm}`,
+        {
+          headers: {
+            "x-rapidapi-key": API_KEY,
+            "x-rapidapi-host": API_HOST,
+          },
+        }
+      )
       .then(function (response) {
-        const drinks = response.data;
-        console.log(drinks);
+        console.log(response.data);
       })
       .catch(function (error) {
         console.error(error);
@@ -36,10 +36,11 @@ export default function SearchScreen() {
       <Input
         placeholder='Search for a cocktail by ingredient'
         autoComplete='off'
-        value={searchTerm}
+        autoCapitalize='none'
+        value={inputValue}
         leftIcon={<Feather name='search' size={24} color='#F4F9E9' />}
         inputStyle={{ color: "#F4F9E9" }}
-        onChange={(searchTerm) => setSearchTerm(searchTerm)}
+        onChange={(event) => setInputValue(event.nativeEvent.text)}
       />
       <Button
         buttonStyle={{ backgroundColor: "#F4F9E9" }}
